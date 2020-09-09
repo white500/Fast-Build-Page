ccaa6f4cdb7b5a8e99e731c2a23af4d6b11a686c
maggot_code <wowhuanhuan233@163.com>
2020-09-09 10:09:53 +0800

    2020年9月9日10:09:49 提交

--- test-demo/src/components/HelloWorld.vue
+++ test-demo/src/components/HelloWorld.vue
@@ -1,25 +1,22 @@
 <template>
-<div>
-
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-   <a-button type="primary">
-      Primary
-    </a-button>
-</div>
+	<h1>{{ msg }}</h1>
+	<button @click="count++">count is: {{ count }}</button>
+	<p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+	<a-button type="primary">
+		Primary
+	</a-button>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
+	name: 'HelloWorld',
+	props: {
+		msg: String
+	},
+	data() {
+		return {
+			count: 0
+		}
+	}
 }
 </script>
--- test-demo/src/main.js
+++ test-demo/src/main.js
@@ -1,12 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-
+import { createApp } from 'vue';
 import Antd from 'ant-design-vue';
+import App from './App.vue';
+import './index.css';
 import 'ant-design-vue/dist/antd.css';
 
-const app = createApp();
+const app = createApp(App);
 app.config.productionTip = false;
-app.use(Antd);
-
-createApp(App).mount('#app');
+app.use(Antd)
+app.mount('#app')
