5ae1553aedd35dce5e12c248ee541d12b99df0f4
maggot_code <wowhuanhuan233@163.com>
2020-09-09 21:30:22 +0800

    提交更新eslint配置；新增ant框架

--- core-app/.eslintrc.js
+++ core-app/.eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/standard',
+		'eslint:recommended'
+	],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-tabs': 'off'
+	}
 }
