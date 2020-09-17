cfe0d3aa45c83af4183d4a5423e378c20df237f5
maggot_code <wowhuanhuan233@163.com>
2020-09-18 00:19:00 +0800

    更新eslint

--- core-app/.eslintrc.js
+++ core-app/.eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true
-	},
-	extends: [
-		'plugin:vue/essential',
-		'@vue/standard',
-		'eslint:recommended'
-	],
-	parserOptions: {
-		parser: 'babel-eslint'
-	},
-	rules: {
-		indent: ['error', 'tab'],
-		semi: 'off',
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-tabs': 'off',
-		'no-trailing-spaces': 'off',
-		'no-unused-vars': 'off',
-		'space-before-function-paren': 'off',
-		'no-useless-constructor': 'off',
-		'no-prototype-builtins': 'off'
-	}
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        indent: ['error', 4],
+        semi: 'off',
+        quotes: 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-tabs': 'off',
+        'no-trailing-spaces': 'off',
+        'no-unused-vars': 'off',
+        'space-before-function-paren': 'off',
+        'no-useless-constructor': 'off',
+        'no-prototype-builtins': 'off'
+    }
 }
--- core-app/src/router/index.js
+++ core-app/src/router/index.js
@@ -13,17 +13,17 @@ import routes from './routes/index'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-	next()
+    next()
 })
 
 router.afterEach(() => {
-	window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 })
 
 export default router
