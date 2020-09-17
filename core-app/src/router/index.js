f134f9abbc6914fddb7d8c3ad0730772d5cab562
white500 <964142664@qq.com>
2020-09-17 22:35:36 +0800

    加了个分号

--- core-app/src/router/index.js
+++ core-app/src/router/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-09 22:45:35
+ * @LastEditTime: 2020-09-17 22:34:49
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \core-app\src\router\index.js
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes/index'
@@ -15,7 +23,7 @@ router.beforeEach((to, from, next) => {
 })
 
 router.afterEach(() => {
-	window.scrollTo(0, 0)
+	window.scrollTo(0, 0);
 })
 
 export default router
